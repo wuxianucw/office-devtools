48a1415c722e90cda73d625dd9d080f2b469419f
无限UCW <x-wuxian@qq.com>
2021-09-29 17:56:22 +0800

    add `try-catch` to avoid aborting

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,23 +2,31 @@ const request = require('superagent');
 
 module.exports = {
     async getToken(id, secret, token) {
-        const res = await request
-            .post("https://login.microsoftonline.com/common/oauth2/v2.0/token")
-            .type("form")
-            .send({
-                grant_type: "refresh_token",
-                refresh_token: token,
-                client_id: id,
-                client_secret: secret,
-                redirect_uri: "http://localhost:53682/"
-            });
-        return res.body;
+        try {
+            const res = await request
+                .post("https://login.microsoftonline.com/common/oauth2/v2.0/token")
+                .type("form")
+                .send({
+                    grant_type: "refresh_token",
+                    refresh_token: token,
+                    client_id: id,
+                    client_secret: secret,
+                    redirect_uri: "http://localhost:53682/"
+                });
+            return res.body;
+        } catch (err) {
+            return err;
+        }
     },
     async request(url, token) {
-        const res = await request
-            .get(url)
-            .set("Authorization", token)
-        return res;
+        try {
+            const res = await request
+                .get(url)
+                .set("Authorization", token)
+            return res;
+        } catch (err) {
+            return err;
+        }
     },
     sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
